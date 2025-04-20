bd00959f2d3867d302ca4cf6712e9d0bdfd5583d
SubhanAmjad2002 <105309274+SubhanAmjad2002@users.noreply.github.com>
2025-04-20 11:00:45 +0500

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DB_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
